c8dd76c09d1d9e9cd7aa476d4219f9b0def0284a
inorf <113130007+inorf@users.noreply.github.com>
2025-11-04 12:03:19 +0000

    added form to app.jsx & start to style in page.module.css (JS code will be moved to another file, css the same)

--- app/page.js
+++ app/page.js
@@ -1,64 +1,38 @@
-import Image from "next/image";
+
+// import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
+        <div className={styles.DivLog}>
+          <h3>
+            DOCS COMPILANCE
+          </h3>
+          <h4 className={styles.welc}>
+            Welcome Back!
+          </h4>
           <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
+            Sing in to start save your time & money.
           </p>
+          <div>
+            <form className={styles.LogForm}>
+              <label>
+                Login
+              </label>
+              <input type="text"></input>
+              <label>
+                P@ssword
+              </label>
+              <input type="password"></input>
+            </form>
+          </div>
         </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className={styles.DivDesc}>
+          <p>
+            Docs Compliance - kompleksowy system do zarządzania wszystkimi terminami i wymaganiami regulacyjnymi Twojej firmy
+          </p>
         </div>
       </main>
     </div>
