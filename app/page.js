a1ea5d19c4017b612ce49d3cc0c4f991f8699a95
inorf <113130007+inorf@users.noreply.github.com>
2025-11-05 21:58:22 +0000

    moved login page to login.js, made a button which make visible login form and new folder for styles, some corrections to the style, added "Return" button

--- app/page.js
+++ app/page.js
@@ -1,38 +1,29 @@
+"use client";
 
-// import Image from "next/image";
-import styles from "./page.module.css";
+import React, { useState } from 'react';
+import Login from '@/components/auth/login';
+import "./globals.css";
 
 export default function Home() {
+
+  const [showLogin, setShowLogin] = useState(false);
+  const handleToggleLogin = () => {
+    setShowLogin(!showLogin);
+  };
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <div className={styles.DivLog}>
-          <h2>
-            DOCS COMPILANCE
-          </h2>
-          <h4 className={styles.welc}>
-            Welcome Back!
-          </h4>
-          <p>
-            Sing in to start save your time & money.
-          </p>
-          <div>
-            <form className={styles.LogForm}>
-              <input placeholder="Login" type="text"></input>
-              <input placeholder="P@ssword" type="password"></input>
-              <button type="submit">Sign In</button>
-            </form>
-          </div>
-        </div>
-        <div className={styles.DivDesc}>
-          <p>
-            <span>Docs Compliance</span> - kompleksowy system do zarządzania wszystkimi terminami i wymaganiami regulacyjnymi Twojej firmy.
-          </p>
-          <p className={styles.copy}>
-            copyright © 2025 Docs Compliance
-          </p>
-        </div>
-      </main>
+    <div className="page">
+      <div className="topContent">
+        <button onClick={handleToggleLogin}>
+          {showLogin ? "Скрыть форму входа" : "Показать форму входа"}
+        </button>
+      </div>
+      <div className="centerContent">
+        {showLogin && <Login 
+        onToggleVisibility={handleToggleLogin} 
+        isCurrentlyVisible={showLogin}/>}
+      </div>
     </div>
+    
   );
 }
